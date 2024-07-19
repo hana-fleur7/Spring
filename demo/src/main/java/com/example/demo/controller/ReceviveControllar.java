78e008a7d8cab3a46c73b53edf20e8e1c57aa500
ayaka_f <ayaka.f071398@gmail.com>
2024-07-19 17:04:37 +0900

    コミットしました

---
+++ demo/src/main/java/com/example/demo/controller/ReceviveControllar.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.form.ReceiveForm;
+
+@Controller
+@RequestMapping("/receive")
+public class ReceviveControllar {
+
+    @RequestMapping("")
+    public String index(){
+        return "info-form";
+    }
+
+    @RequestMapping("/receive-info")
+    public String receiveInfo(ReceiveForm receiveForm, Model model){
+    model.addAttribute("name" , receiveForm.getName());
+    model.addAttribute("age", receiveForm.getAge());
+        return "finished";
+    }
+}
