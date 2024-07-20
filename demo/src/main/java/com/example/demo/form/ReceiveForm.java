e1c2b14cd2c072f6bea10a79bbb4409a6837b226
ayaka_f <ayaka.f071398@gmail.com>
2024-07-20 14:36:32 +0900

    手直し、練習

--- demo/src/main/java/com/example/demo/form/ReceiveForm.java
+++ demo/src/main/java/com/example/demo/form/ReceiveForm.java
@@ -5,23 +5,23 @@ public class ReceiveForm {
     private String age;
 
     public String getName(){
-        return name;
+        return name;//nameを取得して返すメソッド
     }
 
     public void setName(String name){
-        this.name = name;
+        this.name = name;//取得したnameをセット
 
     }
     
     public String getAge(){
-        return age;
+        return age;//ageを取得して返すメソッド
     }
 
     public void setAge(String age){
-        this.age = age;
+        this.age = age;//取得したageをセット
     }
     
-    @Override
+    @Override//toStringメソッドをオーバーライドして、整列表示させる
     public String toString(){
         return "ReceiveForm[name=" + name + ", age=" + age +"]";
     }
